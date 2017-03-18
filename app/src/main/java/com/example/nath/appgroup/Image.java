fb5f5b3130d4bbe864d9f2e76eb1e2fd0fb272c0
Tarkher <maxime.romeas@laposte.net>
2017-03-18 19:04:18 +0100

    JavaDoc v6

--- app/src/main/java/com/example/nath/appgroup/Image.java
+++ app/src/main/java/com/example/nath/appgroup/Image.java
@@ -4,9 +4,50 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 
+/**
+ * <b>Image contains our implementation of all the basic functionalities that
+ * can be useful to work on images</b>
+ * <p>
+ * Furthermore, this class only contains public methods that are analog to the
+ * the most used on of the Bitmap class.
+ * </p>
+ * <p>
+ * An image is defined by those informations :
+ * <ul>
+ * <li>An array of pixels, likely to be modified entirely</li>
+ * <li>The number of lines (the height) of the image</li>
+ * <li>The number of columns (the width) of the image</li>
+ * </ul>
+ * </p>
+ *
+ * @author Nathan Castets
+ * @version 2.0
+ */
 public class Image implements Cloneable {
+    /**
+    * The one-dimensional array containing the values of the image's pixels.
+    * This array can be changed.
+    * 
+    * @see Image#Image(int[], int, int)
+    * @see Image#setPixels
+    * @see Image#getPixels
+    */
     private int[] pixels;
+    
+     /**
+    * The number of rows in the image, it can be used to identify a given pixel more easily.
+    * 
+    * @see Image#Image(int[], int, int)
+    * @see Image#getHeight
+    */
     private int height;
+    
+    /**
+    * The number of columns in the image, it can be used to identify a given pixel more easily.
+    * 
+    * @see Image#Image(int[], int, int)
+    * @see Image#getWidth
+    */
     private int width;
 
     public Image(int[] pixels, int height, int width) {
@@ -47,7 +88,7 @@ public class Image implements Cloneable {
     }
 
     public Bitmap getBitmap() {
-        return Bitmap.createBitmap(pixels, width, height, Bitmap.Config.RGB_565);
+        return Bitmap.createBitmap(pixels, width, height, Bitmap.Config.ARGB_8888);
     }
 
     public int getWidth() {
